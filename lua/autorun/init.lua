5670ad1ede7994f7c0518be96097ff8761660046
KawaiiBunga <107073565+KawaiiBunga@users.noreply.github.com>
2022-07-11 05:00:22 -0500

    Create init.lua

---
+++ lua/autorun/init.lua
@@ -0,0 +1,25 @@
+KarmaScoreboard = KarmaScoreboard or {}
+
+function KarmaScoreboard.LoadingSV(filePath)
+	if SERVER then
+		include(filePath)
+	end
+end
+
+function KarmaScoreboard.LoadingCL(filePath)
+	AddCSLuaFile(filePath)
+	if CLIENT then
+		include(filePath)
+	end
+end
+
+function KarmaScoreboard.LoadingSH(filePath)
+	AddCSLuaFile(filePath)
+	include(filePath)
+end
+
+KarmaScoreboard.LoadingSH("KarmaScoreboard/configuration.lua")
+KarmaScoreboard.LoadingCL("KarmaScoreboard/cl_scoreboard.lua")
+
+if SERVER then 
+end
